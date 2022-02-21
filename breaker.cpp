e9929fed9e28c4842069cf999ca01fe59ab57903
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-02-21 22:28:36 +0800

    Update breaker.cpp

--- breaker.cpp
+++ breaker.cpp
@@ -1,1 +1,29 @@
-//breaker.cpp
+// breaker.cpp
+
+#include<iostream>
+
+using namespace std;
+
+/*
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+Command switch implementation by Equinoxtic
+Original code sample from project REVENGE
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+*/
+
+void CommandSwitch(int currentCommand)
+{
+    switch (currentCommand)
+    {
+        case 1:
+            break;
+    }
+}
+
+int main()
+{
+    int curOption;
+    cin >> curOption;
+    CommandSwitch(curOption);
+    return 0;
+}
